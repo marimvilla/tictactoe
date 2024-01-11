f2796bb58e64d5ea37c07b9e5e3ee5e7db63188e
Maria Villalobos <villapadita1@gmail.com>
2024-01-11 17:12:07 -0400

    V1 el reset no sirve

---
+++ tictactooe/src/App.jsx
@@ -0,0 +1,86 @@
+import { useState } from 'react'
+import './App.css'
+import confetti from 'canvas-confetti'
+import { Square } from './components/Square.jsx'
+import { TURNS } from './constants.js'
+import { checkWinner, checkEndGame } from './logic/board.js'
+import { WinnerModal } from './components/Winner.jsx'
+import {saveGameToStorage} from './logic/storage/index.js'
+import { resetGameStorage } from './logic/storage/index.js'
+
+
+function App() {
+  const [board, setBoard] = useState(() => {
+    const boardFromStorage = window.localStorage.getItem('board')
+    if (boardFromStorage) return JSON.parse(boardFromStorage)
+    return Array(9).fill(null)
+  })
+
+  const [turn, setTurn] = useState(() => {
+    const turnFromStorage = window.localStorage.getItem('turn')
+    return turnFromStorage ?? TURNS.X
+  })
+
+  const [winner, setWinner] = useState(null)
+
+  const resetGame = () => {
+    resetGameStorage()
+    setBoard(Array(9).fill)
+    setTurn(TURNS.X)
+    setWinner(null)
+
+  }
+
+  const updateBoard = (index) => {
+    if (board[index] || winner) return
+    const newBoard = [...board]
+    newBoard[index] = turn
+    setBoard(newBoard)
+    const newTurn = turn === TURNS.X ? TURNS.O : TURNS.X
+    setTurn(newTurn)
+
+    saveGameToStorage({
+      board: newBoard,
+      turn: newTurn
+    })
+
+    const newWinner = checkWinner(newBoard)
+    if (newWinner) {
+      confetti()
+      setWinner(newWinner)
+  } else if(checkEndGame(newBoard)){
+    setWinner(false)
+  }
+}
+
+  return (
+    <main className='board'>
+    <h1>TicTacToe</h1>
+    <button onClick={resetGame}>Empezar de nuevo</button>
+    <section className='game'>
+      {
+        board.map((square, index) => {
+          return(
+            <Square 
+            key={index}
+            index={index}
+            updateBoard={updateBoard}>
+          
+              {square}
+            </Square>
+          )
+        })
+      }
+    </section>
+
+    <section className='turn'>
+      <Square isSelected={turn === TURNS.X}> {TURNS.X} </Square>
+      <Square isSelected={turn === TURNS.O}> {TURNS.O} </Square>
+    </section>
+
+      <WinnerModal resetGame={resetGame} winner={winner} />
+
+  </main>
+  )
+}
+export default App 
